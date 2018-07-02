0e509fa0e93b5640c8fe9d729398142051b69545
Katjanator <38662652+Katjanator@users.noreply.github.com>
2018-07-02 17:31:48 +0200

    Add files via upload

---
+++ aufrufen file 02.07.18.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 17:21:43 2018
+
+@author: Lenovo
+"""
+
+with open ("Werte mit Komma vom 02.07.18.txt","r") as fo:
+    #immre endungen anfügen 
+    print (fo.read())
+    
